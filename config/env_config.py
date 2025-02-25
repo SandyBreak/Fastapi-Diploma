7b29ccf4c256b1770c6b4682e06f0ecfe7575a0a
SandyBreak <coder471@gmail.com>
2025-02-25 14:14:35 +0300

    ADDed PRIVATE_KEY & PUBLIC_KEY in env_config file

--- config/env_config.py
+++ config/env_config.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 import os
 
-
+load_dotenv()
 
 SMARTCAPTCHA_SERVER_KEY = os.environ.get('SMARTCAPTCHA_SERVER_KEY')
+PRIVATE_KEY = os.environ.get('PRIVATE_KEY')
+PUBLIC_KEY = os.environ.get('PUBLIC_KEY')
