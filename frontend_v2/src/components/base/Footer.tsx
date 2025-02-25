3c0043fd39eb0e26b0bd3f39358194532bc0a43c
SandyBreak <coder471@gmail.com>
2025-02-25 14:29:32 +0300

    ADDed Footer component

---
+++ frontend_v2/src/components/base/Footer.tsx
@@ -0,0 +1,17 @@
+
+import '../../styles/components.css'
+
+
+const Footer = () => {
+  	return (
+    	<>
+    	  	<footer>
+    	    	<div className="copyright large-12">
+    	    		<b>© 2025 Великий Степан Сергеевич, ГБПОУ МО «Колледж «Подмосковье». Дипломная работа. Все права защищены.</b>
+    	    	</div>
+    	  	</footer>
+    	</>
+	)
+}
+
+export default Footer
