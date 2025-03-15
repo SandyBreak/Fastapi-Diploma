dd7682f542c118683573c43ab30dda47552fd82c
SandyBreak <coder471@gmail.com>
2025-03-15 21:08:48 +0300

    ADDed App.tsx

---
+++ frontend_v2/src/App.tsx
@@ -0,0 +1,59 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import AuthenticatedRoute from './pages/control/AuthenticatedRoute';
+import FilteredTableView from './pages/data/FilteredTableView';
+import TableView from './pages/data/TableView';
+import LoginPage from './pages/control/LoginPage';
+
+import NotFoundPage from './pages/NotFoundPage';
+
+import 'foundation-sites/dist/css/foundation.min.css';
+const App: React.FC = () => {
+  const router = createBrowserRouter(
+    [
+      {
+        element: <AuthenticatedRoute />,
+        children: [
+          {
+            path: "/authenticated-route",
+              element: <Home />,
+          },
+          {
+            path: "/authenticated-route/view_table",
+              element: <TableView />,
+          },
+          {
+            path: "/authenticated-route/filter_table",
+              element: <FilteredTableView/>,
+          }
+	      ],
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />
+      },
+    ], 
+    {
+      future: {
+          v7_startTransition: true,
+          v7_fetcherPersist: true,
+          v7_normalizeFormMethod: true,
+          v7_partialHydration: true,
+          v7_skipActionErrorRevalidation: true,
+          v7_relativeSplatPath: true
+      },
+    }
+  );
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default App;
