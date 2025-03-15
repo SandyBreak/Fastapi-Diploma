ee6622b9d13573c13b38d5779d76a63d8847a7e2
SandyBreak <coder471@gmail.com>
2025-03-15 23:46:43 +0300

    Crypto fix

--- frontend_v2/vite.config.ts
+++ frontend_v2/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import crypto from 'crypto';
 
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'global.crypto': crypto, // создаем глобальную переменную для crypto
+    'global': 'window', // возможно это тоже поможет для работы в браузере
+  },
+  build: {
+    target: 'esnext', // указываем современный таргет для сборки
+  },
 })
