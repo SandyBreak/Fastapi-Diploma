a7b614c7fa25cf04e76eba7acd96c5ad88c03fd7
SandyBreak <coder471@gmail.com>
2025-03-15 23:31:30 +0300

    Fix

--- frontend_v2/src/App.tsx
+++ frontend_v2/src/App.tsx
@@ -37,16 +37,7 @@ const App: React.FC = () => {
         element: <NotFoundPage />
       },
     ], 
-    {
-      future: {
-          v7_startTransition: true,
-          v7_fetcherPersist: true,
-          v7_normalizeFormMethod: true,
-          v7_partialHydration: true,
-          v7_skipActionErrorRevalidation: true,
-          v7_relativeSplatPath: true
-      },
-    }
+
   );
 
   return (
