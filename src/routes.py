b8b91183732e5d4882e17bb859bb7cad4d62cb21
SandyBreak <coder471@gmail.com>
2025-03-24 15:11:42 +0300

    ADD prefix /api in connecting router

--- src/routes.py
+++ src/routes.py
@@ -4,7 +4,7 @@ from .routers.database import router as database_router
 from .routers.auth import router as auth_router
 
 def get_apps_router():
-    router = APIRouter()
+    router = APIRouter(prefix='/api')
     router.include_router(auth_router.router)
     router.include_router(database_router.router)
     return router
